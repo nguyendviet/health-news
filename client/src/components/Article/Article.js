0a67d617b3602a5341392b65b34191cf489421ce
Viet Nguyen <vietjul@gmail.com>
2018-01-18 09:04:05 -0500

    final. working. no errors

--- client/src/components/Article/Article.js
+++ client/src/components/Article/Article.js
@@ -5,7 +5,7 @@ import {ListItem} from '../List';
 const Article = ({title, url, id, date, handleClick, buttonText, saved}) => (
   <ListItem>
     <h3>
-      <em>{title}</em>{" "}
+      <p>{title}</p>{" "}
       <span className="btn-group float-right">
         <a className="btn btn-outline-success" href={url} rel="noopener noreferrer" target="_blank">
           View Article
--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -4,7 +4,8 @@ import './Card.css';
 const Card = props => (
     <div className="card border-success mb-3">
         <div className="card-header text-white bg-success border-success">
-            <i className="material-icons">{props.icon}</i> {props.title}
+            <i className="material-icons">{props.icon}</i>
+            <span className="card-title ml-2">{props.title}</span>
         </div>
         <div className="card-body">
             <div className="panel-body">{props.children}</div>
