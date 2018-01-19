47542cba472b39d0b8932c85be3f61d89c3c812a
Viet Nguyen <vietjul@gmail.com>
2018-01-19 16:31:13 -0500

    update

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Jumbotron from "../../components/Jumbotron";
-// import Panel from "../../components/Panel";
 import Card from "../../components/Card";
 import Article from "../../components/Article";
 import Footer from "../../components/Footer";
